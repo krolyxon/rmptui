fb06907cfe9b7bce899697c6495941331fa829b2
krolxon <krolyxon@tutanota.com>
2024-04-28 23:18:48 +0530

    epitome of stupidity

--- src/handler.rs
+++ src/handler.rs
@@ -156,7 +156,7 @@ pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
     } else {
         match key_event.code {
             // Quit
-            KeyCode::Char('q'),
+            KeyCode::Char('q')  => app.quit(),
             KeyCode::Char('c') | KeyCode::Char('C') => {
                 if key_event.modifiers == KeyModifiers::CONTROL {
                     app.quit();
