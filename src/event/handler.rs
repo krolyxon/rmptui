1e02da68ccbf3abc6465a72cc01b3487f2285048
krolxon <krolyxon@tutanota.com>
2024-05-12 21:29:51 +0530

    update songs list on db update

--- src/event/handler.rs
+++ src/event/handler.rs
@@ -130,6 +130,17 @@ pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
             // Update MPD database
             KeyCode::Char('U') => {
                 app.conn.conn.rescan()?;
+
+                // Update the songs list
+                app.conn.songs_filenames = app
+                    .conn
+                    .conn
+                    .listall()
+                    .unwrap()
+                    .into_iter()
+                    .map(|x| x.file)
+                    .collect();
+
                 app.browser.update_directory(&mut app.conn)?;
             }
 
